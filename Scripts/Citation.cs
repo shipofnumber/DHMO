43036996f5fc35a58fcc8c2567bb44b65cea225a
Water <132420237+shipofnumber@users.noreply.github.com>
2026-05-31 18:20:40 +0800

    Delete Scripts directory

--- Scripts/Citation.cs
+++
@@ -1,7 +0,0 @@
-namespace DHMO;
-
-public static class DHMOCitations
-{
-    public static Citation GGD { get; private set; } = new("GooseGooseDuck", null, new ColorTextComponent(Color.white, new RawTextComponent("Goose Goose Duck")), "https://gaggle.fun/goose-goose-duck");
-    public static Citation TownOfUsMira { get; private set; } = new("TownOfUsMira", NebulaAPI.AddonAsset.GetResource("TownOfUsMira.png")?.AsImage(70f), new ColorTextComponent(Color.white, new RawTextComponent("TownOfUsMira")), "https://github.com/AU-Avengers/TOU-Mira");
-}
--- Scripts/Events/PelicanEvent.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace DHMO.Events;
-
-public class PelicanDevourEvent(GamePlayer player, GamePlayer devoured) : Virial.Events.Player.AbstractPlayerEvent(player)
-{
-    public GamePlayer Devoured { get; init; } = devoured;
-}
